0b20906f876c83c5f12e2aef98e4099fa27b6739
otmjka <otmjka@gmail.com>
2021-08-12 01:47:12 +0500

    feat(AnimatedTableBody): animation queue, fade-in/slide-out animations

---
+++ src/components/TransactionHistory/AnimatedTableBody.tsx
@@ -0,0 +1,64 @@
+import { FC, useEffect, useState } from 'react';
+import cn from 'classnames';
+import { v4 as uuidv4 } from 'uuid';
+
+import { TransactionHistoryRecord } from '../../../typings/TransactionHistoryRecord';
+import { TransactionHistoryProps } from './TransactionHistory';
+import TransactionHistoryRecordTableItem from './TransactionHistoryRecordTableItem';
+
+type Records = {
+  id: string;
+  fadeIn: boolean;
+  slideOut: boolean;
+  items: Array<TransactionHistoryRecord>;
+};
+
+const AnimatedTableBody: FC<TransactionHistoryProps> = ({
+  transactionHistoryRecords,
+}) => {
+  const [newRecords, setNewRecords] = useState<Array<Records>>([]);
+  useEffect(() => {
+    setNewRecords((oldRecords) => {
+      const tailRecords = oldRecords.map((oldRecord) => ({
+        ...oldRecord,
+        fadeIn: false,
+        slideOut: true,
+      }));
+      return [
+        {
+          id: uuidv4(),
+          fadeIn: true,
+          slideOut: false,
+          items: transactionHistoryRecords,
+        },
+        ...tailRecords,
+      ];
+    });
+  }, [transactionHistoryRecords]);
+  return (
+    <>
+      {newRecords.map((item) => {
+        return (
+          <div
+            key={item.id}
+            className={cn('transaction-history__table__body', {
+              'fade-in': item.fadeIn,
+              'slide-out': item.slideOut,
+            })}
+            onAnimationEnd={() => {
+              console.log('onAnimation end');
+            }}
+          >
+            {item.items.map((transactionHistoryRecord) => (
+              <TransactionHistoryRecordTableItem
+                key={transactionHistoryRecord.id}
+                data={transactionHistoryRecord}
+              />
+            ))}
+          </div>
+        );
+      })}
+    </>
+  );
+};
+export default AnimatedTableBody;
--- src/components/TransactionHistory/TransactionHistory.tsx
+++ src/components/TransactionHistory/TransactionHistory.tsx
@@ -1,11 +1,9 @@
-import React, { FC, useState } from 'react';
-import cn from 'classnames';
-import TransactionHistoryRecordTableItem from './TransactionHistoryRecordTableItem';
+import React, { FC } from 'react';
 import './TransactionHistory.scss';
 // TODO: where should store types
 import { TransactionHistoryRecord } from '../../../typings/TransactionHistoryRecord';
 import { IntlProvider } from 'react-intl';
-import { useEffect } from 'react';
+import AnimatedTableBody from './AnimatedTableBody';
 
 export interface TransactionHistoryProps {
   transactionHistoryRecords: Array<TransactionHistoryRecord>;
@@ -15,35 +13,13 @@ export interface TransactionHistoryProps {
 const TransactionHistory: FC<TransactionHistoryProps> = ({
   transactionHistoryRecords,
 }) => {
-  const [newRecords, setNewRecords] =
-    useState<Array<TransactionHistoryRecord> | null>(null);
-  useEffect(() => {
-    console.log('1');
-    setNewRecords(transactionHistoryRecords);
-  }, [transactionHistoryRecords]);
-
-  console.log('2');
   // TODO: classnames thema + base classes
   return (
     <IntlProvider locale={'en'} defaultLocale={'en'}>
       <div className="transaction-history">
-        <div
-          className={cn('transaction-history__table__body', 'fade-in', {
-            opacity1: !!newRecords,
-            opacity0: !newRecords,
-          })}
-        >
-          {!!newRecords && (
-            <>
-              {newRecords.map((transactionHistoryRecord) => (
-                <TransactionHistoryRecordTableItem
-                  key={transactionHistoryRecord.id}
-                  data={transactionHistoryRecord}
-                />
-              ))}
-            </>
-          )}
-        </div>
+        <AnimatedTableBody
+          transactionHistoryRecords={transactionHistoryRecords}
+        />
       </div>
     </IntlProvider>
   );
